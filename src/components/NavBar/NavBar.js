6bef24f3062d36df7bdf988d01e10437c6abe12b
MSampedro13 <88289205+MSampedro13@users.noreply.github.com>
2022-10-27 18:47:10 -0300

    first commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useCountMoviesFavorites } from "../../hooks/useCountMoviesFavorites";
+import Logo from "../../Film.png";
+
+import "./Navbar.css";
+
+export default function NavBar() {
+  const { countFav } = useCountMoviesFavorites();
+  return (
+    <header className="navbar">
+      <div>
+        <img
+          id="logoFilm"
+          src={Logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt=""
+        />
+      </div>
+      <nav>
+        <ul className="list">
+          <li className="list-item">
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink to="/favs">{`Favoritas (${countFav})`}</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
